bf75291ac83daa6eba16340d97569c85abae8908
Jordan <51269322+jordann124@users.noreply.github.com>
2019-06-03 20:21:40 +0100

    Add files via upload

---
+++ locales/en.lua
@@ -0,0 +1,17 @@
+Locales['en'] = {
+	['carry_for_leaves'] = 'Press ~INPUT_CONTEXT~ to catch fresh oranges',
+	['take_orange_juice'] = 'Press ~INPUT_CONTEXT~ to make fresh orange juice',
+	['sell_orage_juice'] = 'Press ~INPUT_CONTEXT~ to sell fresh orange juice',
+	['bag_full'] = 'You can not get more oranges because your bag is full.',
+	['take_orage'] = '~y~To Paste Oranges~s~...',
+	['you_do_not_have_enough_oranges'] = 'You do not have enough oranges.',
+	['you_do_not_have_any_more_oranges'] = 'You Do not Have More ~r~Orage~s~',
+	['transform_juice_orage'] = '~y~To Transform For Fresh Orange Juice~s~...',
+	['you_do_not_have_juice_orage'] = 'You Do not Have More ~r~Juice Orage~s~',
+	['sell_juice'] = 'Sell ~y~x1~s~ ~y~Juice Orage~s~',
+	['sell_juice_orange'] = '~g~To Sell Fresh Orange Juice~s~...',
+	-- Blips 
+	['orange_picking'] = 'Oranges Picking Area',
+	['turns_from_juice'] = 'Processing For Orange Juice',
+	['sell_juice_orage_blip'] = 'Orange Juice Sale',
+}
