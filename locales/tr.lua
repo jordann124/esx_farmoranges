54637327057f705e779f2517bcea3a7cf2542c2b
abdulkadiraktas <abdulkadir.aktas@hotmail.com>
2019-06-12 14:12:42 +0300

    Create tr.lua
    added tr lang

---
+++ locales/tr.lua
@@ -0,0 +1,17 @@
+Locales['tr'] = {
+	['carry_for_leaves'] = 'Taze portakal toplamak için ~INPUT_CONTEXT~ tuşuna basın.',
+	['take_orange_juice'] = 'Taze portakal kullanarak meyve suyu yapmak için ~INPUT_CONTEXT~ tuşuna basın.',
+	['sell_orage_juice'] = 'Meyve suyunu satmak için ~INPUT_CONTEXT~ tuşuna basın.',
+	['bag_full'] = 'Çantanız dolu olduğu için daha fazla portakal alamazsınız.',
+	['take_orage'] = '~y~Portakal toplanıyor~s~...',
+	['you_do_not_have_enough_oranges'] = 'Yeteri kadar portakala sahip değilsin.',
+	['you_do_not_have_any_more_oranges'] = 'Daha fazla ~r~portakal~s~ yok.',
+	['transform_juice_orage'] = '~y~Meyve suyuna dönüştürülüyor~s~...',
+	['you_do_not_have_juice_orage'] = 'Daha fazla ~r~Portakal Suyu~s~ yok.',
+	['sell_juice'] = '~y~x1~s~ adet ~y~portakal suyu~s~ satıldı.',
+	['sell_juice_orange'] = '~g~taze Portakal Suyu Satmak İçin~s~...',
+	-- Blips 
+	['orange_picking'] = 'Portakal Toplama Alanı',
+	['turns_from_juice'] = 'Portakal Suyu İşleme',
+	['sell_juice_orage_blip'] = 'Portakal Suyu Satışı',
+}
